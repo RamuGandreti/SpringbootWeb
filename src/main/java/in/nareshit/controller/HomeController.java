a37104e61e1960e066f5a2ec66569539c5922bbd
ramubyjava@gmail.com <ramubyjava@gmail.com>
2021-09-14 23:00:35 +0530

    first commit

---
+++ src/main/java/in/nareshit/controller/HomeController.java
@@ -0,0 +1,13 @@
+package in.nareshit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/show")
+	public String show() {
+	return "HomePage";
+	}
+
+}
